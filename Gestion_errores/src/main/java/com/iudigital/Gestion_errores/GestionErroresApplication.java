d7ff67a93dd1e44dc231d6cbfa9e68b695748111
memoleon0716 <guillermo.loaiza@est.iudigital.edu.co>
2023-10-04 23:09:43 -0500

    Primer comic

---
+++ Gestion_errores/src/main/java/com/iudigital/Gestion_errores/GestionErroresApplication.java
@@ -0,0 +1,15 @@
+package com.iudigital.Gestion_errores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionErroresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionErroresApplication.class, args);
+
+		
+	}
+
+}
